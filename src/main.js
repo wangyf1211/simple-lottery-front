0f890aa51822d71ef2bed2a55d06fe82e0fd1505
wyf <wyf@promote.cache-dns.local>
2019-11-21 23:21:21 +0800

    添加创建抽奖活动页面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { DatePicker, Form, FormItem, Input } from "element-ui";
+import YDUI from "vue-ydui"; /* 相当于import YDUI from 'vue-ydui/ydui.rem.js' */
+import "vue-ydui/dist/ydui.px.css";
+import "element-ui/lib/theme-chalk/index.css";
+/* 使用px：import 'vue-ydui/dist/ydui.px.css'; */
+
+Vue.use(YDUI);
+Vue.use(DatePicker);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
 
 Vue.config.productionTip = false;
 
---
+++ src/views/Join.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="join"></div>
+</template>
+<script>
+export default {
+  name: "join",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="less" scoped></style>
